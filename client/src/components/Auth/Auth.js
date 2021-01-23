67761795c0a424440f46eb4749482e744ca33aab
rjustin16 <justiniano.ricky@gmail.com>
2021-01-22 21:02:47 -0700

    google

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,44 +1,130 @@
-import React from 'react'
-import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import { GoogleLogin } from "react-google-login";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import useStyles from "./styles";
-import Input from "./Input"
+import Input from "./Input";
+import Icon from "./icon"
 
 const Auth = () => {
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSingup, setIsSignup] = useState(false);
 
-    const classes = useStyles();
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword);
 
-    const isSingup = false;
+  const handleSubmit = () => {};
 
-    const handleSubmit = () => {
+  const handleChange = () => {};
 
-    }
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    handleShowPassword(false);
+  };
+  const googleSuccess = (res) =>{
+      console.log(res)
+  }
+  const googleFailure = () =>{
+      console.log("google sign in was unsuccessful.")
+  }
 
-    const handleChange = () => {
-
-    }
-    return (
-       <Container component='main' maxWidth='xs'>
-           <Paper className={classes.paper} elevation={3}>
-            <Avatar className={classes.avatar}>
-                <LockOutlinedIcon/>
-            </Avatar>
-            <Typography variant='h5'>{isSingup ? 'Sign Up' : 'Sign In'}</Typography>
-            <form className={classes.form} onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-                {
-                    isSingup && (
-                        <>
-                        <TextField name="firstName" label='First Name' handleChange={handleChange} autoFocus xs={6}/>
-                        <TextField name="lastName" label='Last Name' handleChange={handleChange} autoFocus xs={6}/>
-                        </>
-                    )
-                }
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSingup ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSingup && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSingup && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <GoogleLogin
+            clientId="GOOGLE ID"
+            render={(renderProps) => (
+              <Button
+                className={classes.googleButton}
+                color="primary"
+                fullWidth
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                startIcon={<Icon />}
+                variant="contained"
+              >
+                  Google Sign In
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy='single_host_origin'
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSingup ? "Sign Up" : "Sign In"}
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSingup
+                  ? "Already have an account? Sigh In"
+                  : "Don't have an account? Sign Up"}
+              </Button>
             </Grid>
-            </form>
-           </Paper>
-       </Container>
-    )
-}
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
 
-export default Auth
+export default Auth;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -16,17 +16,37 @@ const Navbar = () => {
         <img className={classes.image} src={icon} alt="icon" height="60" />
             </div>
             <Toolbar className={classes.toolbar}>
-            {user ? (
-                <div className={classes.profile}>
-                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                    <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
-                    <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
-                </div>
-            ): (
-                <Button compontent= {Link} to="/auth" variant='contained' color="primary">Sign In</Button>
-
-            )}
-            </Toolbar>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
       </AppBar>
     )
 }
