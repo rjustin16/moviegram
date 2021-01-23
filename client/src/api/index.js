838ca96310ee2f3329238ac3a3775085d397bf68
rjustin16 <justiniano.ricky@gmail.com>
2021-01-22 19:49:51 -0700

    auth

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+// const url = "https://moviegramzw16.herokuapp.com/posts";
 const url = "http://localhost:5000/posts";
+
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`)
-export const dislikePost = (id) => axios.patch(`${url}/${id}/dislikePost`)
-export const watchPost = (id) => axios.patch(`${url}/${id}/watchPost`)
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const dislikePost = (id) => axios.patch(`${url}/${id}/dislikePost`);
+export const watchPost = (id) => axios.patch(`${url}/${id}/watchPost`);
--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
+app.get('/', (req, res) => {
+  res.send('hello moviegramzw16');
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
